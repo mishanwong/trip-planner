64388f35bb1250faa70e6e1b576b349cfad54ac4
mishanwong <mishan.wong@gmail.com>
2021-10-21 16:55:20 -0700

    First working version

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
-import urllib.request, json
+import requests, math
+from geopy.distance import geodesic
+from itertools import permutations
 
 app = Flask(__name__)
+
+NUM_CITIES = 4
 
 
 @app.route("/")
@@ -15,22 +19,111 @@ def capital():
         country_code = request.form.get("code")
         url = f"http://api.worldbank.org/v2/country/{country_code}?format=json"
 
-        response = urllib.request.urlopen(url)
-        data = response.read()
-        dict = json.loads(data)
-        return render_template("capital.html", capital=dict)
+        r = requests.get(url)
+        data = r.json()[1]
+        capital = data[0]["capitalCity"]
+
+        return render_template("capital.html", data=capital)
+
     else:
         return render_template("capital.html")
 
 
-@app.route("/coordinates")
+@app.route("/coordinates", methods=["GET", "POST"])
 def coordinates():
     if request.method == "POST":
-        return render_template("coordinates.html")
+
+        min_lat = request.form.get("min-lat")
+        max_lat = request.form.get("max-lat")
+        min_lng = request.form.get("min-lng")
+        max_lng = request.form.get("max-lng")
+
+        url = f"http://api.worldbank.org/v2/country/all?format=json"
+
+        r = requests.get(url)
+        data = r.json()[1]
+        result = []
+
+        for country in data:
+            if (
+                country["longitude"] > min_lng
+                and country["longitude"] < max_lng
+                and country["latitude"] > min_lat
+                and country["latitude"] < max_lat
+            ):
+                result.append(country["capitalCity"])
+
+        return render_template("coordinates.html", data=result)
     else:
         return render_template("coordinates.html")
 
 
-@app.route("/route")
+@app.route("/route", methods=["GET", "POST"])
 def route():
-    return "Route"
+    if request.method == "POST":
+        # city_1 = request.form.get("city-1")
+        # city_2 = request.form.get("city-2")
+        # city_3 = request.form.get("city-3")
+        # city_4 = request.form.get("city-4")
+        city_0 = "Kuala Lumpur"
+        city_1 = "Brasilia"
+        city_2 = "Bangkok"
+        city_3 = "New Delhi"
+
+        url = f"http://api.worldbank.org/v2/country/all?format=json&per_page=500"
+
+        r = requests.get(url)
+        data = r.json()[1]
+
+        for country in data:
+            if country["capitalCity"] == city_0:
+                coord_0 = (float(country["latitude"]), float(country["longitude"]))
+
+            if country["capitalCity"].lower() == city_1.lower():
+                coord_1 = (float(country["latitude"]), float(country["longitude"]))
+
+            if country["capitalCity"].lower() == city_2.lower():
+                coord_2 = (float(country["latitude"]), float(country["longitude"]))
+
+            if country["capitalCity"].lower() == city_3.lower():
+                coord_3 = (float(country["latitude"]), float(country["longitude"]))
+
+        # Calculate distance between all cities - total 6 distances and save it in a matrix
+        d01 = geodesic(coord_0, coord_1).miles
+        d02 = geodesic(coord_0, coord_2).miles
+        d03 = geodesic(coord_0, coord_3).miles
+        d12 = geodesic(coord_1, coord_2).miles
+        d13 = geodesic(coord_1, coord_3).miles
+        d23 = geodesic(coord_2, coord_3).miles
+
+        # This can be improved
+        distance_matrix = [
+            [0, d01, d02, d03],
+            [d01, 0, d12, d13],
+            [d02, d12, 0, d23],
+            [d03, d13, d23, 0],
+        ]
+
+        list_of_routes = []
+
+        perm = permutations([0, 1, 2, 3])
+
+        for tuple in list(perm):
+            distance = 0
+            for j in range(3):
+                distance += distance_matrix[tuple[j]][tuple[j + 1]]
+
+            list_of_routes.append([tuple, round(distance, 2)])
+
+        path = None
+        min_distance = math.inf
+
+        for route in list_of_routes:
+            if route[1] < min_distance:
+                min_distance = route[1]
+                path = route[0]
+
+        print(path)
+        return render_template("routes.html")
+    else:
+        return render_template("routes.html")
---
+++ helper.py
@@ -0,0 +1,73 @@
+# from geopy.distance import geodesic
+
+# city_1 = "Kuala Lumpur"
+# city_2 = "Brasilia"
+# city_3 = "Bangkok"
+# city_4 = "New Delhi"
+
+
+# dict = {}
+
+# dict["12"] = {
+#     "startCity": city_1,
+#     "endCity": city_2,
+#     "distance": geodesic(coord_1, coord_2).miles,
+# }
+
+# dict["13"] = {
+#     "startCity": city_1,
+#     "endCity": city_3,
+#     "distance": geodesic(coord_1, coord_3).miles,
+# }
+
+# dict["14"] = {
+#     "startCity": city_1,
+#     "endCity": city_4,
+#     "distance": geodesic(coord_1, coord_4).miles,
+# }
+
+# dict["23"] = {
+#     "startCity": city_2,
+#     "endCity": city_3,
+#     "distance": geodesic(coord_2, coord_3).miles,
+# }
+
+# dict["24"] = {
+#     "startCity": city_2,
+#     "endCity": city_4,
+#     "distance": geodesic(coord_2, coord_4).miles,
+# }
+
+# dict["34"] = {
+#     "startCity": city_3,
+#     "endCity": city_4,
+#     "distance": geodesic(coord_3, coord_4).miles,
+# }
+
+# print(dict)
+[
+    [(0, 1, 2, 3), 22352.22],
+    [(0, 1, 3, 2), 20870.1],
+    [(0, 2, 1, 3), 19922.33],
+    [(0, 2, 3, 1), 11400.12],
+    [(0, 3, 1, 2), 21570.13],
+    [(0, 3, 2, 1), 14530.04],
+    [(1, 0, 2, 3), 12749.86],
+    [(1, 0, 3, 2), 14397.66],
+    [(1, 2, 0, 3), 13449.88],
+    [(1, 2, 3, 0), 14530.04],
+    [(1, 3, 0, 2), 11967.77],
+    [(1, 3, 2, 0), 11400.12],
+    [(2, 0, 1, 3), 19789.95],
+    [(2, 0, 3, 1), 11967.77],
+    [(2, 1, 0, 3), 22919.87],
+    [(2, 1, 3, 0), 21570.13],
+    [(2, 3, 0, 1), 14397.66],
+    [(2, 3, 1, 0), 20870.1],
+    [(3, 0, 1, 2), 22919.87],
+    [(3, 0, 2, 1), 13449.88],
+    [(3, 1, 0, 2), 19789.95],
+    [(3, 1, 2, 0), 19922.33],
+    [(3, 2, 0, 1), 12749.86],
+    [(3, 2, 1, 0), 22352.22],
+]
